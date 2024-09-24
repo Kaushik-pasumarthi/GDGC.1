bd57698c8b69b130ec68cc6363910279846499f0
Kaushik-pasumarthi <142872376+Kaushik-pasumarthi@users.noreply.github.com>
2024-09-25 00:41:19 +0530

    Update thinks.js

--- thinks.js
+++ thinks.js
@@ -1,12 +1,12 @@
 let cart = [];
-const productSection = document.getElementById('product-section');
-const cartItems = document.getElementById('cart-items');
-const totalMrpEl = document.getElementById('total-mrp');
-const totalAmountEl = document.getElementById('total-amount');
+const productSection=document.getElementById('product-section');
+const cartItems=document.getElementById('cart-items');
+const totalMrpEl=document.getElementById('total-mrp');
+const totalAmountEl=document.getElementById('total-amount');
 
-const discount = 50;
-const platformFee = 10;
-const shippingCharges = 20;
+const discount=30;
+const platformFee=10;
+const shippingCharges=20;
 
 
 
@@ -18,7 +18,7 @@ fetch('https://fakestoreapi.com/products')
 
 function displayProducts(output) {
     output.forEach(output => {
-        const productCard = document.createElement('div');
+        const productCard=document.createElement('div');
         productCard.classList.add('product-card');
         
         productCard.innerHTML = `
@@ -33,22 +33,22 @@ function displayProducts(output) {
     });
 }
 function filterProducts() {
-    const searchTerm = document.getElementById('search-box').value.toLowerCase();
-    const productCards = document.querySelectorAll('.product-card');
+    const searchTerm=document.getElementById('search-box').value.toLowerCase();
+    const productCards=document.querySelectorAll('.product-card');
     productCards.forEach(card => {
-        const title = card.querySelector('h3').textContent.toLowerCase();
-        card.style.display = title.includes(searchTerm) ? '' : 'none';
+        const title=card.querySelector('h3').textContent.toLowerCase();
+        card.style.display=title.includes(searchTerm) ? '' : 'none';
     });
 }
 
 
-function addToCart(id, title, price, image) {
+function addToCart(id,title,price,image) {
     const existingItem = cart.find(item => item.id === id);
     
     if (existingItem) {
         existingItem.quantity += 1;
     } else {
-        cart.push({ id, title, price, image, quantity: 1 });
+        cart.push({ id,title,price,image,quantity: 1 });
     }
     
     renderCart();
@@ -75,7 +75,7 @@ function renderCart() {
 }
 
 function removeFromCart(id) {
-    cart = cart.filter(item => item.id !== id);
+    cart=cart.filter(item => item.id !== id);
     renderCart();
     calculateTotal();
 }
